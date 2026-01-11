d9bb0d7c8a9b257eef85f67a45110efaa2490ca5
Colin Liu <colin198702@gmail.com>
2026-01-11 12:09:21 +0800

    Fix issue in vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,10 @@ import renderer from 'vite-plugin-electron-renderer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true, // Fail if port 5173 is not available
+  },
   plugins: [
     electron([
       {
